1ce83143d35ae19c57dca4b2430a200e4ed5019a
Pariwat <wat_promjai@hotmail.com>
2014-10-21 00:28:58 +0700

    update

---
+++ app/src/main/java/com/theweatherhere/HandleJSON.java
@@ -0,0 +1,116 @@
+package com.theweatherhere;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+
+public class HandleJSON {
+
+    private String name = "name";
+    private String main = "main";
+    private String temp = "temp";
+    private String sunrise = "sunrise";
+    private String sunset = "sunset";
+    private String humidity = "humidity";
+    private String wind = "wind";
+    private String cloudiness = "cloudiness";
+    private String urlString = null;
+
+    public volatile boolean parsingComplete = true;
+    public HandleJSON(String url){
+        this.urlString = url;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getMain(){
+        return main;
+    }
+    public String getTemp(){
+        return temp;
+    }
+    public String getSunrise(){
+        return sunrise;
+    }
+    public String getSunset(){
+        return sunset;
+    }
+    public String getHumidity(){
+        return humidity;
+    }
+    public String getWind(){
+        return wind;
+    }
+    public String getCloudiness(){
+        return cloudiness;
+    }
+
+    @SuppressLint("NewApi")
+    public void readAndParseJSON(String in) {
+        try {
+            JSONObject reader = new JSONObject(in);
+
+            name = reader.getString("name");
+
+            //JSONObject weatherApi  = reader.getJSONObject("weather");
+            //main = weatherApi.getString("main");
+
+            JSONObject mainApi  = reader.getJSONObject("main");
+            temp = mainApi.getString("temp");
+            humidity = mainApi.getString("humidity");
+
+            JSONObject sysApi  = reader.getJSONObject("sys");
+            sunrise = sysApi.getString("sunrise");
+            sunset = sysApi.getString("sunset");
+
+            JSONObject windApi  = reader.getJSONObject("wind");
+            wind = windApi.getString("speed");
+
+            JSONObject cloudsApi  = reader.getJSONObject("clouds");
+            cloudiness = cloudsApi.getString("all");
+
+            parsingComplete = false;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    public void fetchJSON(){
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(urlString);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000 /* milliseconds */);
+                    conn.setConnectTimeout(15000 /* milliseconds */);
+                    conn.setRequestMethod("GET");
+                    conn.setDoInput(true);
+                    // Starts the query
+                    conn.connect();
+                    InputStream stream = conn.getInputStream();
+
+                    String data = convertStreamToString(stream);
+
+                    readAndParseJSON(data);
+                    stream.close();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+    }
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+}
--- app/src/main/java/com/theweatherhere/MainActivity.java
+++ app/src/main/java/com/theweatherhere/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -12,10 +14,23 @@ public class MainActivity extends Activity {
     // GPSTracker class
     GPSTracker gps;
 
+    private String url = "http://api.openweathermap.org/data/2.5/weather?lat=18.7992741&lon=98.9742345";
+    private TextView name,main,temp,sunrise,sunset,humidity,wind,cloudiness;
+    private HandleJSON obj;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (TextView) findViewById(R.id.name);
+        main = (TextView) findViewById(R.id.main);
+        temp = (TextView) findViewById(R.id.temp);
+        sunrise = (TextView) findViewById(R.id.sunrise);
+        sunset = (TextView) findViewById(R.id.sunset);
+        humidity = (TextView) findViewById(R.id.humidity);
+        wind = (TextView) findViewById(R.id.wind);
+        cloudiness = (TextView) findViewById(R.id.cloudiness);
 
         // create class object
         gps = new GPSTracker(MainActivity.this);
@@ -23,16 +38,33 @@ public class MainActivity extends Activity {
         if(gps.canGetLocation()){
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
-            // \n is for new line
-            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+
+            String lat = Double.toString(latitude);
+            String lon = Double.toString(longitude);
+
+            String finalUrl = url;
+            obj = new HandleJSON(finalUrl);
+            obj.fetchJSON();
+
+            while(obj.parsingComplete);
+
+            name.setText(obj.getName());
+            main.setText(obj.getMain());
+            temp.setText(obj.getTemp());
+            sunrise.setText(obj.getSunrise());
+            sunset.setText(obj.getSunset());
+            humidity.setText(obj.getHumidity());
+            wind.setText(obj.getWind());
+            cloudiness.setText(obj.getCloudiness());
+
         }else{
             // can't get location
             // GPS or Network is not enabled
             // Ask user to enable GPS/network in settings
             gps.showSettingsAlert();
         }
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
