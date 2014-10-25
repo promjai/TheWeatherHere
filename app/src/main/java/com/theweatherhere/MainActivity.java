1f17e8225c6280c8b9fff330c8820104aa35ab6a
Pariwat <wat_promjai@hotmail.com>
2014-10-25 23:52:09 +0700

    update code

--- app/src/main/java/com/theweatherhere/MainActivity.java
+++ app/src/main/java/com/theweatherhere/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -126,6 +128,27 @@ public class MainActivity extends Activity {
             gps.showSettingsAlert();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_share) {
+            return true;
+        } else if (id == R.id.action_refresh) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
